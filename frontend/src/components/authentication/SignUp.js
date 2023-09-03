fa000553c471047025e06e5b8350f58d5e2725b6
Lena-Kalmikov <100858741+Lena-Kalmikov@users.noreply.github.com>
2023-09-03 23:19:48 +0300

    Sign up with react-hook-form

--- frontend/src/components/authentication/SignUp.js
+++ frontend/src/components/authentication/SignUp.js
@@ -14,6 +14,8 @@ import Typography from "@mui/material/Typography";
 import Container from "@mui/material/Container";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 
+import { useForm, SubmitHandler } from "react-hook-form";
+
 // TODO remove, this demo shouldn't need to reset the theme.
 
 // const defaultTheme = createTheme();
