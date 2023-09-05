166f2c72a6f2f32af8d296f5ded2d0d3c2d3f837
Lena-Kalmikov <100858741+Lena-Kalmikov@users.noreply.github.com>
2023-09-05 23:57:48 +0300

    Update SignUp.js

--- frontend/src/components/authentication/SignUp.js
+++ frontend/src/components/authentication/SignUp.js
@@ -12,7 +12,7 @@ import Box from "@mui/material/Box";
 import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
 import Typography from "@mui/material/Typography";
 import Container from "@mui/material/Container";
-import { createTheme, ThemeProvider } from "@mui/material/styles";
+// import { createTheme, ThemeProvider } from "@mui/material/styles";
 
 import { useForm, SubmitHandler } from "react-hook-form";
 
